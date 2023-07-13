4545454c1d5dc07bcd8565294c1d0ee6c3755233
Spencer Auslander <spencerjauslander@gmail.com>
2023-07-13 14:45:57 -0400

    Thursday Commit

---
+++ PyBank/main.py
@@ -0,0 +1,62 @@
+import os
+import csv
+
+budget_csv = os.path.join('resources', 'budget_data.csv')
+results_csv = os.path.join( "analysis", "results.txt")
+Total_Months = 0
+Total = 0
+Sum_Change = 0
+Greatest_IN = 0
+Greatest_DE = 0
+IN_row = 0
+DE_row = 0
+prerow = 0
+
+with open(budget_csv, 'r', encoding='UTF-8') as budget:
+    budgetreader = csv.reader(budget, delimiter= ",")
+    # https://stackoverflow.com/questions/3392397/python-how-do-i-always-start-from-the-second-row-in-csv code credit
+    # skips the header of csv file
+    next(budgetreader)
+    for row in budgetreader:
+        #counting each month
+        Total_Months += 1
+        #running total of the profit/losses
+        Total += int(row[1])
+        #check to see if this is the first month or entry. if so, just store the row value for the change calculation
+        if Total_Months == 1:
+            prerow = int(row[1])
+        #calculate the change by taking the current profit/loss and subtracting it from last month's profit/loss
+        #add that change to a running total
+        else:
+            Current_Change = int(row[1]) - prerow
+            Sum_Change += Current_Change
+            #If change variable is the greatest increase so far, retrieve the date and set change as the max
+            #Same logic applies to greatest decrease so far
+            if Current_Change > Greatest_IN:
+                IN_row = row[0]
+                Greatest_IN = Current_Change
+            elif Current_Change < Greatest_DE:
+                DE_row = row[0]
+                Greatest_DE = Current_Change
+        #set the current row as the previous row for the next loop calculation
+        prerow = int(row[1])
+    #aftering exiting the loop, calcuate the average change with the final totals
+    Average_Change = round(Sum_Change / (Total_Months-1), 2)
+with open(results_csv, 'w') as results:
+    #printing results in gitbash
+    print('Financial Analysis')
+    print('----------------------------')
+    print('Total Months: ' +  str(Total_Months))
+    print('Total: $' + str(Total))
+    print('Average Change: $' + str(Average_Change))
+    print('Greatest Increase in Profits: ' + str(IN_row) + ' $(' + str(Greatest_IN) + ')')
+    print('Greatest Decrease in Profits: ' + str(DE_row) + ' $(' + str(Greatest_DE) + ')')
+    #writing results in text file
+    resultsvwriter = csv.writer(results, delimiter=',')
+    resultsvwriter.writerow(['Financial Analysis'])
+    resultsvwriter.writerow(['----------------------------'])
+    resultsvwriter.writerow(['Total Months: ' +  str(Total_Months)])
+    resultsvwriter.writerow(['Total: $' + str(Total)])
+    resultsvwriter.writerow(['Average Change: $' + str(Average_Change)])
+    resultsvwriter.writerow(['Greatest Increase in Profits: ' + str(IN_row) + ' $(' + str(Greatest_IN) + ')'])
+    resultsvwriter.writerow(['Greatest Decrease in Profits: ' + str(DE_row) + ' $(' + str(Greatest_DE) + ')'])
---
+++ PyPoll/main.py
@@ -0,0 +1,64 @@
+import os
+import csv
+
+poll_csv = os.path.join('resources', 'election_data.csv')
+results_csv = os.path.join( "analysis", "results.txt")
+name1 = 'Charles Casper Stockham'
+name2 = "Diana DeGette"
+name3 = 'Raymon Anthony Doane'
+vote1 = 0
+vote2 = 0
+vote3 = 0
+totalvotes = 0
+winner = ""
+
+print('Election Results')
+print('----------------------------')
+
+with open(poll_csv, 'r', encoding='UTF-8') as election1: 
+    electionreader1 = csv.reader(election1, delimiter= ",")
+    next(electionreader1)
+    for row in electionreader1:
+        totalvotes += 1
+print(f'Total votes: {totalvotes}')
+print('----------------------------')
+with open(poll_csv, 'r', encoding='UTF-8') as election2: 
+    electionreader2 = csv.reader(election2, delimiter= ",")
+    # https://stackoverflow.com/questions/3392397/python-how-do-i-always-start-from-the-second-row-in-csv code credit
+    # skips the header of csv file
+    next(electionreader2)
+    for vote in electionreader2:
+        if vote[2] == name1:
+                vote1 += 1
+        elif vote[2] == name2:
+                vote2 += 1
+        elif vote[2] == name3:
+            vote3 += 1
+print (f'{name1}: {round((vote1/totalvotes)*100,3)}% ({vote1})')
+print (f'{name2}: {round((vote2/totalvotes)*100,3)}% ({vote2})')
+print (f'{name3}: {round((vote3/totalvotes)*100,3)}% ({vote3})')
+if (vote1 > vote2):
+    if (vote1 > vote3):
+        winner = name1
+    else:
+         winner = name3
+elif (vote2 > vote3):
+        winner = name2
+else:
+     winner = name3       
+print('----------------------------')
+print('Winner: ' + winner)    
+print('----------------------------')
+
+with open(results_csv, 'w') as results:
+    resultsvwriter = csv.writer(results, delimiter=',')
+    resultsvwriter.writerow(['Election Results'])
+    resultsvwriter.writerow(['----------------------------'])
+    resultsvwriter.writerow(['Total votes: {totalvotes}'])
+    resultsvwriter.writerow(['----------------------------'])
+    resultsvwriter.writerow([f'{name1}: {round((vote1/totalvotes)*100,3)}% ({vote1})'])
+    resultsvwriter.writerow([f'{name2}: {round((vote1/totalvotes)*100,3)}% ({vote2})'])
+    resultsvwriter.writerow([f'{name3}: {round((vote1/totalvotes)*100,3)}% ({vote3})'])
+    resultsvwriter.writerow(['----------------------------'])
+    resultsvwriter.writerow([f'Winner: {winner}'])    
+    resultsvwriter.writerow(['----------------------------'])
